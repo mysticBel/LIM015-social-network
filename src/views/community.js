b82079fd52980fa1b6c89d823e5ae2dc8136e39e
mysticBel <dnxbel@gmail.com>
2021-09-18 08:02:36 -0500

    adding uploadImg(), savePost() to show posts in community.js

--- src/views/community.js
+++ src/views/community.js
@@ -1,4 +1,7 @@
 import { currentUser, signOut } from '../scripts/auth.js';
+import { postAdd, getPost } from '../scripts/firestore.js';
+import { imgStorage } from '../scripts/storage.js';
+import { itemPost } from './post.js';
 
 export default (userX) => {
   const viewCommunity = document.createElement('section');
@@ -36,17 +39,21 @@ export default (userX) => {
             <div class="user">
               <img class="avatar-post" src="${userX.photo}"/>
               <p class="name">${userX.username}</p>
+
+              
             </div>
             <div class="content-newpost">
               <form id = "form-post">
                 <textarea class="text-newpost" placeholder="¿What do you want to share today?" spellcheck="false" required></textarea>
-                <img id="post-img" class="post-img" src=""/>
-                <div class="buttons-bar">
-                  <label for="upload-img">
+                <i id = "remove-img" style="display: none" class="fas fa-times-circle"></i>
+                <img id="post-img" class="post-img" src=""/> <label class= "btn-uploadImg" for="upload-img"> Upload an image
                     <input type="file" accept="image/jpeg, image/png, image/gif" id="upload-img" class="upload-img">
-                    <i class="far fa-file-image"><span class="tooltiptext">Upload an image</span></i>
-                  </label>
-                  <button type="submit" id="btn-post" class="btn-post" ><i class="fas fa-paper-plane"></i> Share</button>
+                    <i class="far fa-file-image"></i>
+                   
+                  </label> 
+                <div class="buttons-bar">
+                 
+                  <button type="submit" id="btn-post" class="btn-post" ><i class="fa fa-share-square-o"></i> Share</button>
                 </div>
               </form>
             </div>
@@ -58,6 +65,9 @@ export default (userX) => {
 
         </section>
 
+
+
+
           `;
       const hamburgerBotton = viewCommunity.querySelector('#hamburger-menu');
       const homeNav = viewCommunity.querySelector('#left-menu-header');
@@ -67,12 +77,81 @@ export default (userX) => {
         singOut.classList.toggle('active');
       });
 
-      // Función para cerrar sesión
+      // ===== SIGN OUT =========
       const btnSignOut = viewCommunity.querySelector('#btn-singOut');
       btnSignOut.addEventListener('click', (e) => {
         e.preventDefault();
         window.location.hash = '';
         signOut();
+      });
+      const imagePost = viewCommunity.querySelector('#post-img');
+      const imageDelete = viewCommunity.querySelector('#remove-img');
+      const uploadImg = viewCommunity.querySelector('#upload-img');
+
+      //  ===== UPLOADS AN IMAGE =========
+      uploadImg.addEventListener('change', (e) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = () => {
+          imagePost.src = reader.result;
+        };
+        imageDelete.removeAttribute('style');
+      });
+      //  ===== DELETES IMAGE FROM POST =========
+      imageDelete.addEventListener('click', () => {
+        imagePost.src = '';
+        uploadImg.value = '';
+        imageDelete.style.display = 'none';
+      });
+
+      //  ===== ADDS POST IN FIREBASE =========
+      const savePost = viewCommunity.querySelector('#form-post');
+      savePost.addEventListener('submit', (e) => {
+        e.preventDefault();
+        imagePost.src = '';
+        imageDelete.style.display = 'none';
+        const fileImage = e.target.closest('#form-post').querySelector('input')
+          .files[0];
+        // const load = viewCommunity.querySelector('#uploader');
+        const postText = viewCommunity.querySelector('.text-newpost');
+        // const enterModal = viewCommunity.querySelector('.modal-progress');
+        // const textModal = viewCommunity.querySelector('#messageAlert');
+
+        if (fileImage) {
+          const uploadPost = imgStorage(fileImage, 'Skyy-imgPost');
+          uploadPost.on(
+            'state_changed',
+            // (snapshot) => {
+            // const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            // enterModal.classList.add('showModal');
+            // viewCommunity.querySelector('#closeModal').style.display = 'none';
+            // viewCommunity.querySelector('#uploader').style.display = 'block';
+            // textModal.textContent = 'Success! Your publication is ready :D ';
+            // load.value = progress;
+            // },
+            () => {},
+            () => {
+              uploadPost.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                postAdd(user.uid, postText.value, downloadURL).then(() => {
+                  // enterModal.classList.remove('showModal');
+                  savePost.reset();
+                });
+              });
+            },
+          );
+        } else {
+          postAdd(user.uid, postText.value, '').then(() => {
+            // enterModal.classList.remove('showModal');
+            savePost.reset();
+          });
+        }
+      });
+      const boxPost = viewCommunity.querySelector('#container-post');
+      getPost((post) => {
+        boxPost.innerHTML = '';
+        post.forEach((objPost) => {
+          boxPost.appendChild(itemPost(objPost));
+        });
       });
     } else {
       window.location.hash = '#/';
